5dc48184717e72e709ef95bb1d93b82065168898
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-04-15 22:20:35 -0600

    connection set up

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,17 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '3blindMice',
+    database: 'employees'
+  },
+  console.log('Connected to the employees database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {

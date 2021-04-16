395d6d40443b300e30aa58167c929f84c300ae4c
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-04-15 22:11:30 -0600

    added files and employees

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+  (id, 'Ronald', 'Jones',role_id, manager_id),
+  (id,'Ginny', 'Max', role_id, manager_id),
+  (id,'Bonnie', 'Gaveston', role_id, manager_id),
+  (id,'Charles', 'Parkerson', role_id, manager_id),
+  (id,'Frank', 'Mansfield', role_id, manager_id),
+  (id,'Dora', 'Explorer', role_id, manager_id),
+  (id,'Edward', 'ScissHands', role_id, manager_id),
+  (id,'Sunny', 'Summers', role_id, manager_id),
+  (id,'Octavia', 'Oceania', role_id, manager_id),
+  (id,'Mark', 'Zumm', role_id, manager_id)
+  ;

142599ef77c4a46bf89c78b763b4bd2e8ab58741
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-04-17 11:07:38 -0600

    set up seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,34 @@
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
-  (id, 'Ronald', 'Jones',role_id, manager_id),
-  (id,'Ginny', 'Max', role_id, manager_id),
-  (id,'Bonnie', 'Gaveston', role_id, manager_id),
-  (id,'Charles', 'Parkerson', role_id, manager_id),
-  (id,'Frank', 'Mansfield', role_id, manager_id),
-  (id,'Dora', 'Explorer', role_id, manager_id),
-  (id,'Edward', 'ScissHands', role_id, manager_id),
-  (id,'Sunny', 'Summers', role_id, manager_id),
-  (id,'Octavia', 'Oceania', role_id, manager_id),
-  (id,'Mark', 'Zumm', role_id, manager_id)
-  ;
+  (id, 'Ronald', 'Jones',2, manager_id),
+  (id,'Ginny', 'Max', 8, manager_id),
+  (id,'Bonnie', 'Gaveston', 8, manager_id),
+  (id,'Charles', 'Parkerson', 7, manager_id),
+  (id,'Frank', 'Mansfield', 1, manager_id),
+  (id,'Dora', 'Explorer', 5, manager_id),
+  (id,'Edward', 'ScizzHands', 3, manager_id),
+  (id,'Sunny', 'Summers', 2, manager_id),
+  (id,'Octavia', 'Oceania', 4, manager_id),
+  (id,'Missy', 'Misdimenor', 6, manager_id),
+  (id,'Tom', 'Hanksmen', 4, manager_id),
+  (id,'Mark', 'Zumm', 8, manager_id);
+
+INSERT INTO department (id, department_name)
+VALUES
+  (id, 'Accounting'),
+  (id, 'Public Relations'),
+  (id, 'Engineering'),
+  (id, 'Development'),
+  (id, 'Sales');
+
+INSERT INTO role_employee (id, title, salary, department_id )
+VALUES
+  (id, 'CPA', 100000, 1),
+  (id, 'Accounting Clerk', 45000, 1),
+  (id, 'Lead Engineer', 120000, 2),
+  (id, 'Engineer', 95000, 2),
+  (id, 'Public Relations Manager', 85000, 3),
+  (id, 'HR Manager', 100000, 4),
+  (id, 'Lead Sales', 125000, 5),
+  (id, 'Sales', 80000, 5);
+ 
--- server.js
+++ server.js
@@ -19,17 +19,17 @@ const db = mysql.createConnection(
   console.log('Connected to the employees database.')
 );
 
-db.query(`SELECT * FROM employees`, (err, rows) => {
-  console.log(rows);
-});
+// db.query(`SELECT * FROM employees`, (err, rows) => {
+//   console.log(rows);
+// });
 
-db.query(`SELECT * FROM department`, (err, rows) => {
-  console.log(rows);
-});
+// db.query(`SELECT * FROM department`, (err, rows) => {
+//   console.log(rows);
+// });
 
-db.query(`SELECT * FROM role_employee`, (err, rows) => {
-  console.log(rows);
-});
+// db.query(`SELECT * FROM role_employee`, (err, rows) => {
+//   console.log(rows);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
@@ -44,44 +44,43 @@ function start() {
       type: 'list',
       name: 'FakeCompany Employee Database',
       messasge: "Which would you like to view?",
-      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a role', 'Add an employee', 'Update an employee role'],
+      choices: ['View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role'],
     },
   ]).then(function (response) {
     // console.log("RESP", response.position)
-    if (response.position === 'View all departments') {
+    if (response.choices === 'View all departments') {
       viewDepartments();
     };
   });
 
   // prompt to view all departments
-  function viewDepartments() {
-    console.table.department([
-      {
-        name: 'foo',
-        age: 10
-      }, {
-        name: 'bar',
-        age: 20
-      }
-    ]);
-
-  };
+  
 };
 
-  // will return table departments and department ids
-  // prompt to view all roles
-  // will return a table with job title, role id, department, salary
-  // prompt to view all employees
-  // will return a table of employee data, id, first, last, job title
-  // department, salaries and manager
-  // add a department 
-  // promted to add name of department then will add to table 
-  // add a role 
-  // prompted to enter the name, salary, and department for the role and that role is added to the database
-  // add an employee 
-  // prompted to enter the employee’s first name, last name, role, and manager and that employee is added to the database
-  // update an employee role
-  // prompted to select an employee to update and their new role and this information is updated in the database 
+function viewDepartments() {
+  console.log('it is working')
+
+};
+
+// will return table departments and department ids
+// prompt to view all roles
+// will return a table with job title, role id, department, salary
+// prompt to view all employees
+// will return a table of employee data, id, first, last, job title
+// department, salaries and manager
+// add a department 
+// promted to add name of department then will add to table 
+// add a role 
+// prompted to enter the name, salary, and department for the role and that role is added to the database
+// add an employee 
+// prompted to enter the employee’s first name, last name, role, and manager and that employee is added to the database
+// update an employee role
+// prompted to select an employee to update and their new role and this information is updated in the database 
 
 
 
@@ -111,7 +110,7 @@ function start() {
 
 
 
-  start();
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+start();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
